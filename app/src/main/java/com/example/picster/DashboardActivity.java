c53757abfac77cbc2933cb2a10c9e5a072d30558
akaps98 <fc0302@naver.com>
2023-12-22 18:01:09 +0700

    created activities from navigation bar

--- app/src/main/java/com/example/picster/DashboardActivity.java
+++ app/src/main/java/com/example/picster/DashboardActivity.java
@@ -1,14 +1,47 @@
 package com.example.picster;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class DashboardActivity extends AppCompatActivity {
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setSelectedItemId(R.id.navigation_home);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int selected = item.getItemId();
+
+                if (selected == R.id.navigation_search) {
+                    Intent searchIntent = new Intent(DashboardActivity.this, SearchActivity.class);
+                    startActivityForResult(searchIntent, 1);
+                } else if (selected == R.id.navigation_bookmarks) {
+                    Intent bookmarkIntent = new Intent(DashboardActivity.this, BookmarkActivity.class);
+                    startActivityForResult(bookmarkIntent, 2);
+                } else if (selected == R.id.navigation_home) {
+                    // same activity; stay
+                } else if (selected == R.id.navigation_user) {
+                    Intent myPageIntent = new Intent(DashboardActivity.this, MyPageActivity.class);
+                    startActivityForResult(myPageIntent, 4);
+                } else if (selected == R.id.navigation_setting) {
+                    Intent settingIntent = new Intent(DashboardActivity.this, SettingActivity.class);
+                    startActivityForResult(settingIntent, 5);
+                }
+                return true;
+            }
+        });
     }
 }
