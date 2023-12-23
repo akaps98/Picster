e6a897505c0df59da416872a53906063698a2090
Tony <fc0302@naver.com>
2023-12-23 09:22:23 +0700

    update User model class

--- app/src/main/java/com/example/picster/model/User.java
+++ app/src/main/java/com/example/picster/model/User.java
@@ -1,13 +1,18 @@
 package com.example.picster.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String email, username;
     private boolean VIP; // isPaid?
+    private List<User> friends;
 
     public User(String email) {
         this.email = email;
         this.username = "";
         this.VIP = false;
+        this.friends = new ArrayList<>();
     }
 
     public User(String email, String username) {
@@ -37,5 +42,13 @@ public class User {
 
     public void setVIP(boolean VIP) {
         this.VIP = VIP;
+    }
+
+    public List<User> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<User> friends) {
+        this.friends = friends;
     }
 }
