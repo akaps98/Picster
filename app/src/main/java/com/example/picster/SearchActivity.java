7dc9dc0ae9c9b20def8ae70ce3623b6dc4309da6
Tony <fc0302@naver.com>
2024-01-15 04:05:12 +0700

    search feature is done

--- app/src/main/java/com/example/picster/SearchActivity.java
+++ app/src/main/java/com/example/picster/SearchActivity.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -21,15 +22,21 @@ import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.example.picster.model.Feed;
 import com.example.picster.model.User;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class SearchActivity extends AppCompatActivity {
     BottomNavigationView navigationView;
@@ -37,6 +44,7 @@ public class SearchActivity extends AppCompatActivity {
     RadioGroup searchOptions;
     SearchView searchView;
     ListView searchListView;
+    List<Feed> feeds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,8 +82,6 @@ public class SearchActivity extends AppCompatActivity {
         searchView = findViewById(R.id.searchView);
         searchListView = findViewById(R.id.searchListView);
 
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -91,6 +97,7 @@ public class SearchActivity extends AppCompatActivity {
                     RadioButton selected = findViewById(selectedRadioButtonId);
 
                     String selectedOption = selected.getText().toString();
+                    String lowerCaseSearchText = searchText.toLowerCase(Locale.getDefault());
 
                     if (selectedOption.equals("Friend")) {
                         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -100,27 +107,67 @@ public class SearchActivity extends AppCompatActivity {
 
                         if (currentUser != null) {
                             db.collection("User")
-                                    .whereGreaterThanOrEqualTo("username", searchText)
-                                    .whereLessThanOrEqualTo("username", searchText + "\uf8ff")
                                     .get()
                                     .addOnCompleteListener(task -> {
                                         if (task.isSuccessful()) {
-                                            List<User> userList = new ArrayList<>();
+                                            List<User> searchedUserList = new ArrayList<>();
 
                                             for (DocumentSnapshot document : task.getResult()) {
                                                 User searchedUser = document.toObject(User.class);
                                                 if (searchedUser != null && !searchedUser.getEmail().equals(currentUser.getEmail())) {
-                                                    userList.add(searchedUser);
+                                                    if (searchedUser.getUsername().toLowerCase().contains(lowerCaseSearchText)) {
+                                                        searchedUserList.add(searchedUser);
+                                                    }
                                                 }
                                             }
 
-                                            UserListAdapter userListAdapter = new UserListAdapter(SearchActivity.this, userList);
-                                            searchListView.setAdapter(userListAdapter);
+                                            if (searchedUserList.isEmpty()) {
+                                                Toast.makeText(SearchActivity.this, "No users found", Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                UserListAdapter userListAdapter = new UserListAdapter(SearchActivity.this, searchedUserList);
+                                                searchListView.setAdapter(userListAdapter);
+                                            }
                                         } else {
-                                            Toast.makeText(SearchActivity.this, "Error fetching user data.", Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(SearchActivity.this, "Error", Toast.LENGTH_SHORT).show();
                                         }
                                     });
                         }
+                    } else {
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                        db.collection("Feed")
+                                .get()
+                                .addOnSuccessListener(queryDocumentSnapshots -> {
+                                    feeds = new ArrayList<>();
+                                    for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                                        Feed feed = document.toObject(Feed.class);
+
+                                        if (feed.getContent().toLowerCase().contains(lowerCaseSearchText)) {
+                                            feeds.add(feed);
+                                        }
+                                    }
+
+                                    if (feeds.isEmpty()) {
+                                        Toast.makeText(SearchActivity.this, "No feeds found", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        FeedAdapter adapter = new FeedAdapter(SearchActivity.this, R.layout.home_feed_list_item, feeds);
+                                        searchListView.setAdapter(adapter);
+
+                                        searchListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                            @Override
+                                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                Feed clickedFeed = feeds.get(position);
+
+                                                Intent intent = new Intent(SearchActivity.this, FriendFeedActivity.class);
+                                                intent.putExtra("clickedFeed", clickedFeed);
+                                                startActivity(intent);
+                                            }
+                                        });
+                                    }
+                                })
+                                .addOnFailureListener(e -> {
+                                    Toast.makeText(SearchActivity.this, "Error; " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                });
                     }
                 }
             }
@@ -204,6 +251,41 @@ public class SearchActivity extends AppCompatActivity {
             });
 
             return itemView;
+        }
+    }
+
+    private class FeedAdapter extends ArrayAdapter<Feed> {
+
+        public FeedAdapter(Context context, int resource, List<Feed> feeds) {
+            super(context, resource, feeds);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.home_feed_list_item, parent, false);
+            }
+
+            Feed currentFeed = getItem(position);
+
+            ImageView feedImage = convertView.findViewById(R.id.feedImage);
+            TextView feedUser = convertView.findViewById(R.id.feedUser);
+            TextView feedText = convertView.findViewById(R.id.feedText);
+            TextView likeNumber = convertView.findViewById(R.id.likeNumber);
+            TextView commentNumber = convertView.findViewById(R.id.commentNumber);
+            TextView feedDate = convertView.findViewById(R.id.feedDate);
+
+            Glide.with(SearchActivity.this).load(currentFeed.getImageUri()).into(feedImage);
+            feedUser.setText(currentFeed.getUsername());
+            feedText.setText(currentFeed.getContent());
+            likeNumber.setText(String.valueOf(currentFeed.getLikes()));
+            commentNumber.setText(String.valueOf(currentFeed.getComments().size()));
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd", Locale.getDefault());
+            String formattedDate = sdf.format(new Date(Long.parseLong(currentFeed.getDate())));
+            feedDate.setText(formattedDate);
+
+            return convertView;
         }
     }
 }
